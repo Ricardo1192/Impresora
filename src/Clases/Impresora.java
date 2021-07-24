01f4d47fb86aca548c306bf68336a1d10328c770
danydlpr <danieldelapavar@gmail.com>
2021-07-23 19:23:59 -0500

    getters y setters

--- src/Clases/Impresora.java
+++ src/Clases/Impresora.java
@@ -11,6 +11,30 @@ public class Impresora {
         this.escaner = escaner;
     }
 
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public boolean isEscaner() {
+        return escaner;
+    }
+
+    public void setEscaner(boolean escaner) {
+        this.escaner = escaner;
+    }
+
 
     
 }
