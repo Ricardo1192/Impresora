2cb380479c518084b391675d03991c35bedc927e
Ricardo1192 <ricardo.1701810348@ucaldas.edu.co>
2021-07-23 19:25:56 -0500

    creacion clase DatosImpresoras

--- src/Clases/DatosImpresoras.java
+++ src/Clases/DatosImpresoras.java
@@ -1,5 +1,25 @@
 package Clases;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatosImpresoras {
-    
+    public DatosImpresoras(){
+
+    }
+    private List<Impresora> impresoras= new ArrayList<Impresora>();
+
+    public List<Impresora> consultaImpresoras( ){
+        return impresoras;
+    }
+    public Impresora buscaImpresora(String serial){
+        for (Impresora impresora : impresoras) {
+            if (impresora.getSerial()== serial) {
+                return impresora;
+            }
+        }
+    }
+        
+
+
 }
