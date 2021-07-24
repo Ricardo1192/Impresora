ce409fedf30b66962c10e331e641c38e00a9b1eb
danydlpr <danieldelapavar@gmail.com>
2021-07-23 19:21:28 -0500

    constructor impresora

--- src/Clases/Impresora.java
+++ src/Clases/Impresora.java
@@ -4,4 +4,13 @@ public class Impresora {
     private String serial;
     private String marca;
     private boolean escaner;
+    
+    public Impresora(String serial, String marca, boolean escaner) {
+        this.serial = serial;
+        this.marca = marca;
+        this.escaner = escaner;
+    }
+
+
+    
 }
